c50afb4d98cf873f198624db32d963505bfeb7cc
aravind <aravind.boppana@clairovyantsoft.com>
2019-09-19 08:59:17 -0700

    Initial Commit

---
+++ bin/spark_compaction.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+HELP_STR=""" """
+
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIB_DIR="${BIN_DIR}/../lib"
+CONF_DIR="${BIN_DIR}/../conf"
+JAR_FILE_LOCATION="${LIB_DIR}/spark-compaction-jar-with-dependencies.jar"
+APPLICATION_CONF_FILE="${CONF_DIR}/application_configs.json"
+
+COMPACTION_STRATEGY=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['compaction_strategy'];"`
+SOURCE_DATA_LOCATION=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['hdfs']['source_data_location'];"`
+TARGET_DATA_LOCATION=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['hdfs']['target_data_location'];"`
+
+SOURCE_DB=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['source_db'];"`
+SOURCE_TABLE=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['source_table'];"`
+TARGET_DB=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['target_db'];"`
+TARGET_TABLE=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['target_table'];"`
+
+echo "BIN_DIR: ${BIN_DIR}"
+echo "LIB_DIR: ${LIB_DIR}"
+echo "JAR_FILE_LOCATION: ${JAR_FILE_LOCATION}"
+echo "APPLICATION_CONF_FILE: ${APPLICATION_CONF_FILE}"
+echo "COMPACTION STRATEGY: " ${COMPACTION_STRATEGY}
+
+compaction() {
+
+    APP_NAME=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['spark']['app_name'];"`
+    SPARK_MASTER=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['spark']['master'];"`
+
+    echo "APP_NAME: ${APP_NAME}"
+
+    echo "Launching Spark Streaming Application in Yarn Client Mode"
+    SPARK_SUBMIT_STARTUP_CMD="spark2-submit --master yarn --deploy-mode client --driver-class-path ${CONF_DIR}:${JAR_FILE_LOCATION} --class com.clairvoyant.nyu.bigdata.spark.SparkCompaction ${JAR_FILE_LOCATION} "
+
+
+    echo "executing: ${SPARK_SUBMIT_STARTUP_CMD}"
+    eval ${SPARK_SUBMIT_STARTUP_CMD}
+
+}
+
+echo "Starting Compaction"
+
+compaction
+
+if [[ $? = 0 && "${COMPACTION_STRATEGY}" = "rewrite" ]]; then
+    echo "Drop and reload Data"
+    hadoop fs -rm -r ${SOURCE_DATA_LOCATION}
+    hadoop fs -cp /tmp/Spark_Compaction ${SOURCE_DATA_LOCATION}
+    impala-shell -q "INVALIDATE METADATA ${SOURCE_DB}.${SOURCE_TABLE}"
+    impala-shell -q "COMPUTE STATS ${SOURCE_DB}.${SOURCE_TABLE}"
+fi
+
+if [[ $? = 0 && "${COMPACTION_STRATEGY}" = "new" ]]; then
+    echo "Create new external table as old one pointing to new location"
+    hive -e "CREATE EXTERNAL TABLE ${TARGET_DB}.${TARGET_TABLE} LIKE ${SOURCE_DB}.${SOURCE_TABLE} STORED AS PARQUET LOCATION '${TARGET_DATA_LOCATION}' "
+    impala-shell -q "INVALIDATE METADATA ${TARGET_DB}.${TARGET_TABLE}"
+    impala-shell -q "COMPUTE STATS ${TARGET_DB}.${TARGET_TABLE}"
+
+fi
+
+if [[ $? = 0 ]]; then
+    echo "SUCCESSFULLY COMPLETED COMPACTION"
+fi
+
---
+++ src/main/scala/com/clairvoyant/nyu/bigdata/spark/SparkCompaction.scala
@@ -0,0 +1,90 @@
+package com.clairvoyant.nyu.bigdata.spark
+
+import com.typesafe.config.{Config, ConfigFactory, ConfigList}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, LocatedFileStatus, Path}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object SparkCompaction {
+
+    def main(args: Array[String]): Unit = {
+
+        // Load values form the Config file(application.json)
+        val config: Config = ConfigFactory.load("application_configs.json")
+
+        val SPARK_APP_NAME: String = config.getString("spark.app_name")
+        val SPARK_MASTER: String = config.getString("spark.master")
+
+        val SOURCE_DATA_LOCATION_HDFS = config.getString("hdfs.source_data_location")
+        val TARGET_DATA_LOCATION_HDFS = config.getString("hdfs.target_data_location")
+
+        val COMPACTION_STRATEGY = config.getString("compaction.compaction_strategy")
+        val ENABLE_NUM_PARTITIONS = config.getBoolean("compaction.enable_num_partitions")
+        var NUM_PARTITIONS = config.getInt("compaction.num_partitions")
+        val SIZE_RANGES_FOR_COMPACTION: ConfigList = config.getList("compaction.size_ranges_for_compaction")
+        val DECODED_SIZE_RANGES_FOR_COMPACTION: Array[AnyRef] = SIZE_RANGES_FOR_COMPACTION.unwrapped().toArray
+
+        val sparkConf = new SparkConf().setAppName(SPARK_APP_NAME).setMaster(SPARK_MASTER)
+        val spark = SparkSession.builder.config(sparkConf).getOrCreate()
+
+        if (!ENABLE_NUM_PARTITIONS) {
+
+            val hdfs: FileSystem = FileSystem.get(new Configuration())
+
+            def roundUp(d: Double) = math.ceil(d).toInt
+
+            val hadoopPath = new Path(SOURCE_DATA_LOCATION_HDFS)
+
+            val recursive = false
+            val ri = hdfs.listFiles(hadoopPath, recursive)
+            val it = new Iterator[LocatedFileStatus]() {
+                override def hasNext: Boolean = ri.hasNext
+
+                override def next(): LocatedFileStatus = ri.next()
+            }
+
+            var partition_size = 256
+
+            // Materialize iterator
+            val files = it.toList
+            println("No.of files: " + files.size)
+
+            val hdfs_dir_size_in_mb = files.map(_.getLen).sum * 0.00000095367432  // Re-visit This
+            println("Size: " + hdfs_dir_size_in_mb + " MB")
+
+            val hdfs_dir_size_in_gb = hdfs_dir_size_in_mb * 0.001
+            println("Size in GB: " + hdfs_dir_size_in_gb)
+
+            DECODED_SIZE_RANGES_FOR_COMPACTION.foreach(f = map => {
+                val hashMap = map.asInstanceOf[java.util.HashMap[String, Int]]
+
+                val min_size_in_gb = hashMap.get("min_size_in_gb")
+                var max_size_in_gb = hashMap.get("max_size_in_gb")
+
+                if(max_size_in_gb == 0){
+                    max_size_in_gb = hdfs_dir_size_in_gb.toInt
+                }
+
+                if ((min_size_in_gb <= hdfs_dir_size_in_gb) && (max_size_in_gb >= hdfs_dir_size_in_gb)) {
+                    partition_size = hashMap.get("size_after_compaction_in_mb")
+                }
+
+            })
+
+            NUM_PARTITIONS = roundUp(hdfs_dir_size_in_mb / partition_size)
+        }
+        val df = spark.read.parquet(SOURCE_DATA_LOCATION_HDFS)
+
+        if(COMPACTION_STRATEGY == "rewrite"){
+            println("Rewriting Strategy")
+            df.repartition(NUM_PARTITIONS).write.mode("overwrite").parquet("/tmp/Spark_Compaction")
+        }
+        else {
+            println("Writing to new Locaton")
+            df.repartition(NUM_PARTITIONS).write.parquet(TARGET_DATA_LOCATION_HDFS)
+        }
+
+    }
+
+}
