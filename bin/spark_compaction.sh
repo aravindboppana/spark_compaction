01e50718b4a3bba44a3348e577660e47191b44a5
aravind <aravind.boppana@clairovyantsoft.com>
2019-09-19 09:06:42 -0700

    Modifying Shell Script

--- bin/spark_compaction.sh
+++ bin/spark_compaction.sh
@@ -10,6 +10,7 @@ APPLICATION_CONF_FILE="${CONF_DIR}/application_configs.json"
 
 COMPACTION_STRATEGY=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['compaction_strategy'];"`
 SOURCE_DATA_LOCATION=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['hdfs']['source_data_location'];"`
+SOURCE_DATA_BACKUP_LOCATION="${SOURCE_DATA_LOCATION}_backup"
 TARGET_DATA_LOCATION=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['hdfs']['target_data_location'];"`
 
 SOURCE_DB=`cat "$APPLICATION_CONF_FILE" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['compaction']['source_db'];"`
@@ -45,8 +46,13 @@ compaction
 
 if [[ $? = 0 && "${COMPACTION_STRATEGY}" = "rewrite" ]]; then
     echo "Drop and reload Data"
+    hadoop fs -cp ${SOURCE_DATA_LOCATION} ${SOURCE_DATA_BACKUP_LOCATION}
     hadoop fs -rm -r ${SOURCE_DATA_LOCATION}
     hadoop fs -cp /tmp/Spark_Compaction ${SOURCE_DATA_LOCATION}
+    if [[ $? = 0 ]]; then
+        hadop fs -rm -r /tmp/Spark_Compaction
+        hadoop fs -rm -r ${SOURCE_DATA_BACKUP_LOCATION}
+    fi
     impala-shell -q "INVALIDATE METADATA ${SOURCE_DB}.${SOURCE_TABLE}"
     impala-shell -q "COMPUTE STATS ${SOURCE_DB}.${SOURCE_TABLE}"
 fi
